0868edd3e240e0024402dd79949d9fd0d427d82b
tomas <72792707+tomas-land@users.noreply.github.com>
2021-03-12 10:03:44 +0200

    commit1

---
+++ index.php
@@ -0,0 +1,56 @@
+
+
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
+    <title>Document</title>
+</head>
+<body>
+
+
+
+<div class="container px-4">
+        <div class="row gx-5">
+          <div class="col">
+           <div class="p-3  bg-light">
+            <a class="list-group-item list-group-item-action text-center" id="list-profile-list" data-bs-toggle="list" href="<?php echo 'employees.php'?>" role="tab" aria-controls="profile">EMPLOYEES</a>
+           </div>
+          </div>
+          <div class="col">
+            <div class="p-3  bg-light">
+                
+                    <a class="list-group-item list-group-item-action text-center" id="list-messages-list" data-bs-toggle="list" href="<?php echo 'projects.php'?>" role="tab" aria-controls="messages">PROJECTS</a>
+
+                </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+
+      <?php
+
+
+
+    ?>
+
+
+
+
+
+
+</body>
+</html>
+
+
+
+
+
+
