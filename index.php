69a52b11c92e0a5d4af8cef8570c3915104b427d
tomas <72792707+tomas-land@users.noreply.github.com>
2021-03-12 13:16:27 +0200

    commit4

--- index.php
+++ index.php
@@ -1,4 +1,23 @@
+<?php 
 
+$servername = "localhost";
+$username = "root";
+$password = "mysql";
+$dbname = "projekt_man";
+
+// Create connection
+$conn = mysqli_connect($servername, $username, $password, $dbname);
+// Check connection
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+}
+
+
+
+// require 'main_php.php';
+// require 'employees.php'
+// mysqli_close($conn);
+?>
 
 
 
@@ -9,23 +28,30 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
+    <link rel="stylesheet" href="style/styles.css">
     <title>Document</title>
 </head>
 <body>
 
 
+      <?php
+
+
+
+    ?>
 
 <div class="container px-4">
         <div class="row gx-5">
           <div class="col">
            <div class="p-3  bg-light">
-            <a class="list-group-item list-group-item-action text-center" id="list-profile-list" data-bs-toggle="list" href="<?php echo 'employees.php'?>" role="tab" aria-controls="profile">EMPLOYEES</a>
+            <a class="orange list-group-item list-group-item-action text-center" id="list-profile-list" data-bs-toggle="list" href="<?php echo '?employee'?>" role="tab" aria-controls="profile">EMPLOYEES</a>
            </div>
           </div>
           <div class="col">
             <div class="p-3  bg-light">
                 
-                    <a class="list-group-item list-group-item-action text-center" id="list-messages-list" data-bs-toggle="list" href="<?php echo 'projects.php'?>" role="tab" aria-controls="messages">PROJECTS</a>
+                    <a class="list-group-item list-group-item-action text-center" id="list-messages-list" data-bs-toggle="list" href="<?php echo '?projects'?>" role="tab" aria-controls="messages">PROJECTS</a>
 
                 </div>
             </div>
@@ -34,11 +60,22 @@
       </div>
 
 
-      <?php
+<?php
+
+if(isset($_GET['employee'])){
+require 'employees.php';
+}
+
+if(isset($_GET['projects'])){
+  require 'projects.php';
+  }
+
+  ?>
 
 
 
-    ?>
+
+
 
 
 
--- main_php.php
+++ main_php.php
@@ -2,34 +2,8 @@
 
 
 
-$servername = "localhost";
-$username = "root";
-$password = "mysql";
-$dbname = "projekt_man";
-
-// Create connection
-$conn = mysqli_connect($servername, $username, $password, $dbname);
-// Check connection
-if (!$conn) {
-    die("Connection failed: " . mysqli_connect_error());
-}
-
-$sql = "SELECT employee_id, first_name, last_name, project FROM employees";
-$result = mysqli_query($conn, $sql);    
 
 
-function display_employees($result){
-if (mysqli_num_rows($result) > 0) {
-    while($row = mysqli_fetch_assoc($result)) {
-         echo "<tr><td> " . $row["employee_id"]. " </td> ".
-        "<td> " . $row["first_name"]. " </td> ". 
-        "<td> " . $row["last_name"]. " </td> " .
-        "<td> " . $row["project"]. " </td></tr><br> ";
-    }
-} else {
-    echo "0 results";
-}
-}
 
 
 // $sql = "SELECT employee_id, first_name, last_name, project FROM employees";
@@ -43,7 +17,7 @@ if (mysqli_num_rows($result) > 0) {
 //     echo "0 results";
 // }
 // include 'employees.php';
-mysqli_close($conn);
+// mysqli_close($conn);
 
 
 
